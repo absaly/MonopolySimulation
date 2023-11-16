dfb70134955615abe7d29b518cab6c1f6ed4b025
Abbas Al-younis <abbasal-younis@Abbass-MacBook-Air.local>
2023-11-16 16:43:03 -0700

    Modified the card enums to match the assignment and made changes to the card class with a test client in the class.

--- src/monopolySimulation/Card.java
+++ src/monopolySimulation/Card.java
@@ -1,34 +1,70 @@
 package monopolySimulation;
 
-import enums.Action;
+import java.util.ArrayList;
+import java.util.List;
+
+import enums.ChanceCards;
 import enums.DeckType;
 
+/**
+ * Represents a card in the game Monopoly based of the card type.
+ * 
+ * @author Abbas Al-Younis, Tanner Durrant, Erin Mortensen, Trenton Stratton
+ */
 public class Card {
 	
 	private String cardName;
 	private DeckType cardDeckType;
-	private Action cardAction;
 	
-	public Card(String name, DeckType deckType, Action action) {
+	/**
+	 * Constructs a card with its name and deck type.
+	 * 
+	 * @param name
+	 * @param deckType
+	 */
+	public Card(String name, DeckType deckType) {
 		this.cardName = name;
 		this.cardDeckType = deckType;
-		this.cardAction = action;
-		
-		// TODO not sure if complete.
 	}
 
+	/**
+	 * Returns card name.
+	 * 
+	 * @return cardName
+	 */
 	public String getCardName() {
 		return cardName;
 	}
 
+	/**
+	 * Returns deck type of card.
+	 * 
+	 * @return cardDeckType
+	 */
 	public DeckType getCardDeckType() {
 		return cardDeckType;
 	}
-
-	public Action getCardAction() {
-		return cardAction;
-	}
 	
+	/**
+	 * Returns string representation of card.
+	 */
+	@Override
+	public String toString() {
+		return "(" + cardName + ", " + cardDeckType + ")";
+	}
+
+	// Test client of chance deck being created.
+	public static void main(String[] args) {
+		ChanceCards[] chance = ChanceCards.values();
+		
+		List<Card> chanceDeck = new ArrayList<>();
+		
+		for (ChanceCards c : chance) {
+			chanceDeck.add(new Card(c.toString(), DeckType.CHANCE));
+		}
+		
+		System.out.println(chanceDeck);
+	}
 	
 	
 }
